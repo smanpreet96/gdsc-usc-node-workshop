2983baa07418a29ce756d8e708e914aa73b3095d
Manpreet Singh <msingh60@usc.edu>
2021-09-30 13:28:58 -0700

    Fixing issue in app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-    res.statusCode(200);
+    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 });
